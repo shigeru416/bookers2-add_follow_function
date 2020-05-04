92c573670d2f16961cd9308017bffc1e49d6ec50
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-05-04 22:50:45 +0900

    [add]follow_button and [fixed]_profile.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
    resource :book_comments, only:[:create, :destroy]
    resource :favorites, only: [:create, :destroy]
   end
+
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # 
   
 end
